7ad85ce17bc12f7be91da6ab9a456b8a49f9a25d
HaJuegos <hagatoclips69@gmail.com>
2024-09-16 18:30:38 -0500

    Tarea de clase #1

---
+++ generador_contras/main.js
@@ -0,0 +1,34 @@
+const limitInput = document.getElementById('cantidad');
+const contrasena = document.getElementById('contrasena');
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+const hasNums = /\d/;
+const hasSimbols = /[!@#$%^&*()_+\[\]{}|;:,.<>?]/;
+
+function generarContrasena() {
+    const num = parseInt(limitInput.value);
+
+    if (limitInput.value == '') {
+        alert("Debes introducir un número antes de generar una contraseña.");
+    } else {
+        let newPassword = '';
+
+        for (let i = 0; i < num; i++) {
+            let newCharacter = characters[Math.floor(Math.random() * characters.length)];
+            newPassword += newCharacter;
+        };
+
+        contrasena.value = newPassword;
+
+        if (hasSimbols.test(newPassword)) {
+            alert("La contraseña generada es muy fuerte.");
+        } else if (hasNums.test(newPassword)) {
+            alert("La contraseña generada es fuerte.");
+        } else {
+            alert("La contraseña generada es débil.");
+        };
+    };
+};
+
+function limpiarContrasena() {
+    contrasena.value = '';
+};
